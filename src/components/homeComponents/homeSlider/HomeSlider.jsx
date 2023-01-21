b3be7501617a083bc0d4629cb097820485412c5c
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2023-01-21 03:01:45 +0200

    Home first section

--- src/components/homeComponents/homeSlider/HomeSlider.jsx
+++ src/components/homeComponents/homeSlider/HomeSlider.jsx
@@ -1,16 +1,28 @@
 import './homeslider.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
-import {faEnvira} from '@fortawesome/free-brands-svg-icons'
-import {Container, Row, Col} from 'react-bootstrap'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvira } from '@fortawesome/free-brands-svg-icons';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Homeslider = () => {
-
-    return (
-        <div className='home-slider'>
-            
-        </div>
-    )
-}
+  return (
+    <div
+      className='banner'
+      style={{
+        background: 'url("./imgs/home/BannerBK.png") no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <h1 className='banner__header--l1'>Learn a ton while</h1>
+      <h1 className='banner__header--l2'>having fun</h1>
+      <p className='banner__para'>
+        Forget the old rules. You can have the best <br></br> learn . Right now.
+        Right here.
+      </p>
+      <button className='btn-solid'>Join for Free</button>
+      <button className='btn-outline'>Log In</button>
+    </div>
+  );
+};
 
 export default Homeslider;
