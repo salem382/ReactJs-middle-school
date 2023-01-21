ca4dc4fd22c2726c99c9c1777b775e0a98d4f7e0
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2023-01-21 08:03:18 +0200

    Teachers First section

--- src/components/teachersComponent/teacherslider/Teacherslider.jsx
+++ src/components/teachersComponent/teacherslider/Teacherslider.jsx
@@ -1,23 +1,22 @@
 import './teacherslider.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
-import {faEnvira} from '@fortawesome/free-brands-svg-icons'
-import {Container, Row, Col} from 'react-bootstrap'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvira } from '@fortawesome/free-brands-svg-icons';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Teacherslider = () => {
-
-    return (
-        <div>
-            Teacherslider
-            {
-                /*
-                 example for fontawesome icon
-                    
-                 <FontAwesomeIcon icon={faUser}/>
-                */
-            }
-        </div>
-    )
-}
+  return (
+    <div
+      className='teacher-header'
+      style={{
+        background:
+          'url("./imgs/teachers/sectionImg.png") no-repeat, linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(.jpg)',
+        backgroundSize: 'cover',
+      }}
+    >
+      <h1 className='title'>Our Teachers</h1>
+    </div>
+  );
+};
 
 export default Teacherslider;
