d7d7d529f0ca8b26b489414389201df78867a7a5
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2023-01-21 08:58:44 +0200

    teachers page

--- src/components/teachersComponent/teacherPersons/TeacherPerson.jsx
+++ src/components/teachersComponent/teacherPersons/TeacherPerson.jsx
@@ -1,23 +1,56 @@
 import './teacherpersons.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
-import {faEnvira} from '@fortawesome/free-brands-svg-icons'
-import {Container, Row, Col} from 'react-bootstrap'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvira } from '@fortawesome/free-brands-svg-icons';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Teacherpersons = () => {
-
-    return (
-        <div>
-            teacherpersons
-            {
-                /*
-                 example for fontawesome icon
-                    
-                 <FontAwesomeIcon icon={faUser}/>
-                */
-            }
-        </div>
-    )
-}
+  return (
+    <div className='teachers'>
+      <div className='teacher'>
+        <img src='./imgs/teachers/teacherImg.png' alt='' />
+        <h4 className='name'>Mr. Hashem hany</h4>
+        <p className='title'>Biology teacher</p>
+        <span className='school'>middle School</span>
+        <button className='action'>view lessons</button>
+      </div>
+      <div className='teacher'>
+        <img src='./imgs/teachers/teacherImg.png' alt='' />
+        <h4 className='name'>Mr. Hashem hany</h4>
+        <p className='title'>Biology teacher</p>
+        <span className='school'>middle School</span>
+        <button className='action'>view lessons</button>
+      </div>
+      <div className='teacher'>
+        <img src='./imgs/teachers/teacherImg.png' alt='' />
+        <h4 className='name'>Mr. Hashem hany</h4>
+        <p className='title'>Biology teacher</p>
+        <span className='school'>middle School</span>
+        <button className='action'>view lessons</button>
+      </div>
+      <div className='teacher'>
+        <img src='./imgs/teachers/teacherImg.png' alt='' />
+        <h4 className='name'>Mr. Hashem hany</h4>
+        <p className='title'>Biology teacher</p>
+        <span className='school'>middle School</span>
+        <button className='action'>view lessons</button>
+      </div>
+      <div className='teacher'>
+        <img src='./imgs/teachers/teacherImg.png' alt='' />
+        <h4 className='name'>Mr. Hashem hany</h4>
+        <p className='title'>Biology teacher</p>
+        <span className='school'>middle School</span>
+        <button className='action'>view lessons</button>
+      </div>
+      <div className='teacher'>
+        <img src='./imgs/teachers/teacherImg.png' alt='' />
+        <h4 className='name'>Mr. Hashem hany</h4>
+        <p className='title'>Biology teacher</p>
+        <span className='school'>middle School</span>
+        <button className='action'>view lessons</button>
+      </div>
+    </div>
+  );
+};
 
 export default Teacherpersons;
