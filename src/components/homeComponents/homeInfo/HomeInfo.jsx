67c32f2646c35b433e307037e8bc6f885d3ab63a
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2023-01-21 03:47:45 +0200

    Second section

--- src/components/homeComponents/homeInfo/HomeInfo.jsx
+++ src/components/homeComponents/homeInfo/HomeInfo.jsx
@@ -1,16 +1,47 @@
 import './homeinfo.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
-import {faEnvira} from '@fortawesome/free-brands-svg-icons'
-import {Container, Row, Col} from 'react-bootstrap'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvira } from '@fortawesome/free-brands-svg-icons';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Homeinfo = () => {
-
-    return (
-        <div>
-           Home info
-        </div>
-    )
-}
+  return (
+    <div className='info'>
+      <img className='info__img' src='./imgs/home/people.png' alt='' />
+      <div className='content'>
+        <h1 className='content__header'>
+          Lorem ipsum dolor amet, consectetur{' '}
+        </h1>
+        <p className='content__para'>
+          Lorem ipsum dolor sit amet, consectetur dipiscing elit eius mod tempor
+          incididunt ut
+        </p>
+        <ul className='features'>
+          <li className='feature'>
+            <div className='icon'>
+              <img src='./imgs/home/clockIcon.png' alt='' />
+            </div>
+            <div className='title'>Quiz after the lesson</div>
+            <div className='para'>Lorem ipsum dolor sit amet</div>
+          </li>
+          <li className='feature'>
+            <div className='icon'>
+              <img src='./imgs/home/peopleIcon.png' alt='' />
+            </div>
+            <div className='title'>Many Teachers</div>
+            <div className='para'>Lorem ipsum dolor sit amet</div>
+          </li>
+          <li className='feature'>
+            <div className='icon'>
+              <img src='./imgs/home/enjoyIcon.png' alt='' />
+            </div>
+            <div className='title'>Enjoy learning</div>
+            <div className='para'>Lorem ipsum dolor sit amet</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Homeinfo;
